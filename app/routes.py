edca37e3a41525842298e72ea3922c18c6c8e791
Tiago Vinicius <vinicius.remigio@gmail.com>
2024-05-29 20:05:38 -0300

    chore(Logger): add logger

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,11 @@ from fastapi import APIRouter, HTTPException
 
 from app.model import Recipe
 from app.service import RecipeService
+from app.logger import Logger
 
 service = RecipeService()
 router = APIRouter()
+logger = Logger()
 
 
 @router.get("/")
@@ -18,5 +20,7 @@ def hello():
 async def get_recipes_by_user(user_id: str) -> Optional[List[Recipe]]:
     recipes = service.list_recipes_by_user_id(user_id=user_id)
     if not recipes:
+        logger.warning(f"recipes=not_found")
         raise HTTPException(status_code=404, detail="Recipes not found")
+    logger.info(f"recipes={recipes}")
     return recipes
